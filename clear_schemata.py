f8736061e940a6dd6fd8904a94804b5a59bf25c3
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2025-05-01 09:54:07 -0400

    Initial version

---
+++ clear_schemata.py
@@ -0,0 +1,18 @@
+#! /usr/local/bin/python3
+"""Drop all schemata created by mk_tables."""
+
+import psycopg
+
+if __name__ == '__main__':
+  with psycopg.connect('dbname=cuny_curriculum') as conn:
+    with conn.cursor() as cursor:
+      cursor.execute("""
+      select schema_name
+        from information_schema.schemata
+       where schema_name ~* '^a20' -- names are “aYYYY-MM-DD”, so this selects all in 21st century
+       """)
+      rows = cursor.fetchall()
+      for row in rows:
+        schema_name = row[0]
+        print(f'Drop {schema_name}')
+        cursor.execute(f'drop schema {schema_name} cascade')
